aa480a2d36c256620e19e47215a20a85190362f8
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-02-09 09:34:30 -0600

    Added comments to ValidatingFormInputApplication.java.

--- src/main/java/com/tts/validatingforminput/ValidatingFormInputApplication.java
+++ src/main/java/com/tts/validatingforminput/ValidatingFormInputApplication.java
@@ -11,3 +11,15 @@ public class ValidatingFormInputApplication {
 	}
 
 }
+
+
+/*
+	To activate Spring MVC, you would normally add @EnableWebMvc to the Application class,
+	but Spring Boot's @SpringBootApplication already adds this annotation when it detects
+	spring-webmvc on your classpath. This same annotation lets it find the annotated
+	@Controller class and its methods.
+
+	Thymeleaf configuration is also taken care of by @SpringBootApplication. By default,
+	templates are located in the classpath under templates/ and are resolved as view by
+	stripping the '.html' suffix off the file name.
+ */
